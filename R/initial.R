fce04ba7ed21fb6ae0a059fd0a45e2a630cf6065
Steffen Greilich <s.greilich@dkfz.de>
2015-05-21 00:00:00 +0000

    version 0.6.1

--- R/initial.R
+++ R/initial.R
@@ -2,7 +2,7 @@
  library.dynam("libamtrack", pkg, lib)
 }
 .onAttach <- function(lib, pkg){
- packageStartupMessage("This is libamtrack 0.6.0 'Black Armadillo' (2015-05-21).\nType '?libamtrack' for help.\n")
+ packageStartupMessage("This is libamtrack 0.6.1 'Blue Armadillo' (2015-05-21).\nType '?libamtrack' for help.\n")
 }
 .onUnload <- function(libpath){
  try(library.dynam.unload("libamtrack", libpath))
--- src/config.h
+++ src/config.h
@@ -2,7 +2,7 @@
 /* config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* Cryptonym to faciliate versioning for users */
-#define CODE_NAME "Black Armadillo"
+#define CODE_NAME "Blue Armadillo"
 
 /* Status of the code */
 #define CODE_STATUS "Release"
@@ -91,7 +91,7 @@
 #define PACKAGE_NAME "libamtrack"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libamtrack 0.6.0"
+#define PACKAGE_STRING "libamtrack 0.6.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "libamtrack"
@@ -100,16 +100,16 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.6.0"
+#define PACKAGE_VERSION "0.6.1"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* repository svn version */
-#define SVN_VERSION "1335:1345M"
+#define SVN_VERSION "1335:1357M"
 
 /* Version number of package */
-#define VERSION "0.6.0"
+#define VERSION "0.6.1"
 
 /* Define for Solaris 2.5.1 so the uint32_t typedef from <sys/synch.h>,
    <pthread.h>, or <semaphore.h> is not used. If the typedef were allowed, the
